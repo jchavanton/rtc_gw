d7e5e4806445e065b68af223f6c4853d2c5504be
root <root@c82cfc42493a>
2018-05-25 23:47:41 +0000

    run script

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-./out/Default/rtc_gw --port 9999 --listen 147.75.69.51
+#!/bin/sh
+
+
+
+echo 10 > /proc/sys/net/ipv4/tcp_fin_timeout
+echo 1  > /proc/sys/net/ipv4/tcp_tw_recycle
+echo 1  > /proc/sys/net/ipv4/tcp_tw_reuse
+
+cd ../..
+./out/Default/rtc_gw --port 9999 --listen 147.75.38.39
