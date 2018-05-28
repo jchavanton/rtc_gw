643cfe59cc13c978549c2a234b3bf1f55fd81716
root <root@c82cfc42493a>
2018-05-28 23:53:50 +0000

    run and build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 cd ../..
-pwd
+SRC_DIR=`pwd`
+export PATH=$PATH:$SRC_DIR/../../depot_tools
+echo $PATH
 ninja -C out/Default
--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-
-
 echo 10 > /proc/sys/net/ipv4/tcp_fin_timeout
 echo 1  > /proc/sys/net/ipv4/tcp_tw_recycle
 echo 1  > /proc/sys/net/ipv4/tcp_tw_reuse
-
 cd ../..
-./out/Default/rtc_gw --port 9999 --listen 147.75.38.39
+IP=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
+CMD="./out/Default/rtc_gw --port 9999 --listen $IP"
+echo $CMD
+exec $CMD
